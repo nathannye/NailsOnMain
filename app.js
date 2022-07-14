414ee9488134a0c16bd277aeccbc675ff856a08e
nathannye <79227199+nathannye@users.noreply.github.com>
2022-07-13 22:32:39 -0400

    markup + init

--- app.js
+++ app.js
@@ -7,12 +7,24 @@ const port = 3000;
 app.set('views', './views', 'views');
 app.set('view engine', 'pug');
 
-app.get('/about', (req, res) => {
-  res.render('pages/about');
+app.get('/', (req, res) => {
+  res.render('pages/home');
 });
 
 app.get('/home', (req, res) => {
   res.render('pages/home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+app.get('/services', (req, res) => {
+  res.render('pages/services');
+});
+
+app.get('/our-team', (req, res) => {
+  res.render('pages/our-team');
 });
 
 app.listen(port, () => {
