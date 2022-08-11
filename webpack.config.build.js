a3fba186330469c80e730ab3cc59fffe77498591
nathannye <79227199+nathannye@users.noreply.github.com>
2022-08-11 12:47:31 -0400

    destructured merge import

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -2,6 +2,7 @@ import path from 'path';
 import config from './webpack.config.js';
 import { fileURLToPath } from 'url';
 import { merge } from 'webpack-merge';
+const { merge } = merge;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
