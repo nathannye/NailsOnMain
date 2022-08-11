db584bc95e08b7600bdc5f01887ce898de8369af
nathannye <79227199+nathannye@users.noreply.github.com>
2022-08-11 12:27:04 -0400

    fixed dirname undefined webpack

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,6 +1,9 @@
 import merge from 'webpack-merge';
 import path from 'path';
 import config from './webpack.config.js';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const buildCOnfig = merge(config, {
   mode: 'production',
