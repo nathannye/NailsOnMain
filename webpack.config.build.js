30afb4176f63b1d425b66e618b604097a5642986
nathannye <79227199+nathannye@users.noreply.github.com>
2022-08-11 12:34:22 -0400

    fixed speling of buildconfig

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -2,11 +2,10 @@ import path from 'path';
 import config from './webpack.config.js';
 import { fileURLToPath } from 'url';
 import { merge } from 'webpack-merge';
-import config from './webpack.config';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const buildCOnfig = merge(config, {
+const buildConfig = merge(config, {
   mode: 'production',
 
   output: {
